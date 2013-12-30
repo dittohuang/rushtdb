75c4eac96e348c5d9f969af96a557d766b8b4880
dittohuang <dittohuang@gmail.com>
2013-12-30 16:58:07 +0800

    Update rushtdb.js

--- rushtdb.js
+++ rushtdb.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 //
 // @name			Rush TDB
-// @namespace		http://RushTDB/
-// @description		Rush TDB
+// @namespace			http://RushTDB/
+// @version			20131230.1
+// @description			Rush TDB
 // @include			https://kyfw.12306.cn/otn/leftTicket/init
 // @require			http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 //
