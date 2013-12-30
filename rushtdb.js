f0c47d2bf1d764b3c832e297500d3a154ac9c6a4
dittohuang <dittohuang@gmail.com>
2013-12-30 17:00:58 +0800

    Update rushtdb.js

--- rushtdb.js
+++ rushtdb.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 //
-// @name			Rush TDB
-// @namespace			http://RushTDB/
-// @version			20131230.1
-// @description			Rush TDB
-// @include			https://kyfw.12306.cn/otn/leftTicket/init
-// @require			http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @name                        Rush TDB
+// @namespace                   http://RushTDB/
+// @version                     20131230.1
+// @description                 Rush TDB
+// @include                     https://kyfw.12306.cn/otn/leftTicket/init
+// @require                     http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 //
 // ==/UserScript==
 
 
 
 $(document).ready(function(){
-	unsafeWindow.autoSearchTime = 1000;
+        unsafeWindow.autoSearchTime = 1000;
 });
